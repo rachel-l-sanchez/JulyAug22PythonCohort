cf6ff84ad80a1c4bb148fb4c5fffc8b9b0792a17
Rachel Sanchez <rachel.sanchez@live.com>
2022-07-21 08:02:32 -0700

    updated

--- assignments/week3/booksCore/booksschema.sql
+++ assignments/week3/booksCore/booksschema.sql
@@ -21,12 +21,13 @@ USE `books_schema` ;
 DROP TABLE IF EXISTS `books_schema`.`books` ;
 
 CREATE TABLE IF NOT EXISTS `books_schema`.`books` (
-  `book_id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(255) NOT NULL,
   `num_of_pages` INT NULL,
   `created_at` DATETIME NULL,
   `updated_at` DATETIME NULL,
-  PRIMARY KEY (`book_id`))
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
@@ -36,11 +37,11 @@ ENGINE = InnoDB;
 DROP TABLE IF EXISTS `books_schema`.`users` ;
 
 CREATE TABLE IF NOT EXISTS `books_schema`.`users` (
-  `user_id` INT NOT NULL AUTO_INCREMENT,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NULL,
-  `updated_at` DATETIME NOT NULL,
+  `updated_at` DATETIME NULL,
   `create_at` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user_id`));
+  PRIMARY KEY (`id`));
 
 
 -- -----------------------------------------------------
@@ -56,12 +57,12 @@ CREATE TABLE IF NOT EXISTS `books_schema`.`favorites` (
   INDEX `fk_users_has_books_users_idx` (`user_id` ASC) VISIBLE,
   CONSTRAINT `fk_users_has_books_users`
     FOREIGN KEY (`user_id`)
-    REFERENCES `books_schema`.`users` (`user_id`)
+    REFERENCES `books_schema`.`users` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_users_has_books_books1`
     FOREIGN KEY (`book_id`)
-    REFERENCES `books_schema`.`books` (`book_id`)
+    REFERENCES `books_schema`.`books` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
@@ -69,16 +70,20 @@ CREATE TABLE IF NOT EXISTS `books_schema`.`favorites` (
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+SET SQL_SAFE_UPDATES = 0;
 
-insert into users (user_id,name, updated_at)
-values ('1','Jane Amsden', current_timestamp()), ('2','Emily Dixon', current_timestamp()), ('3','Theodore Dostoevsky', current_timestamp()), ('4','William Shapiro', current_timestamp()), ('5','Lao Xiu', current_timestamp());
+insert into users (name)
+values ('Jane Amsden'), ('Emily Dixon'), ('Theodore Dostoevsky'), ('William Shapiro'), ('Lao Xiu');
 
-insert into books (book_id, title)
-values ('1','C Sharp'), ('2','Java'), ('3','Python'), ('4','PHP'), ('5','Ruby');
+insert into books (id, title, user_id)
+values (1,'C Sharp', 1), (2,'Java', 3), (3,'Python',2), (4,'PHP',1), (5,'Ruby',5);
 
-UPDATE books SET title ="C#" WHERE book_id='1';
+INSERT into favorites (user_id, book_id) 
+VALUES (1, 1), (1, 2), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3), (3, 4), (4, 1), (4, 2), (4, 3), (4, 4), (4,5);
 
-Update users set name="Bill" where user_id='4';
+UPDATE books SET title ="C#" WHERE book_id=1;
+
+Update users set name="Bill" where user_id=4;
 
 update favorites set user_id=1 where book_id <=2;
 
@@ -88,20 +93,23 @@ update favorites set user_id=3 where book_id <5;
 
 update favorites set user_id=4 where book_id <=5;
 
-select user_id
-from favorites
+select users.name from users
+join favorites on users.id = user_id
+left join books on favorites.book_id=books.id
 where book_id= 3;
 
 delete from favorites
 where book_id = 3 and user_id = 1;
 
 insert into favorites (book_id, user_id)
-values('2','5');
+values(2,5);
 
-select book_id
-from favorites
-where user_id = 3;
+select books.title from books
+join favorites as faves on faves.book_id = books.id
+where faves.user_id = 3;
 
-select user_id
-from favorites
-where book_id = 5;
+select name from users
+join favorites on users.id = favorites.user_id;
+
+select * from books
+where books.id = 4;
