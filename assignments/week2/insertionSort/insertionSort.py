7e001426a4b757b63c816141750ec4e98c4de596
Rachel Sanchez <rachel.sanchez@live.com>
2022-07-24 14:07:03 -0700

    init

--- assignments/week2/insertionSort/insertionSort.py
+++ assignments/week2/insertionSort/insertionSort.py
@@ -1,10 +1,23 @@
-def selectionSort(arr):
-    for i in range(0,len(arr)):
-        min_idx = i
-        for j in range(i+1, len(arr)):
-            if arr[j]< arr[min_idx]:
-                min_idx = j
-            arr[min_idx], arr[i] = arr[i], arr[min_idx] 
+arr = [3, 7, 25, 10, 5]
+
+def bubbleSort(arr):
+    for i in range(0, len(arr)-1):
+        for k in range(i+1, len(arr)):
+            if arr[i] > arr[k]:
+                arr[i], arr[k] = arr[k], arr[i]
     return arr
 
-print(selectionSort(arr))
+print(bubbleSort(arr))
+
+def insertionSort(arr):
+    for i in range(1, len(arr)):
+            key = arr[i]
+            j = i-1
+            while key < arr[j] and j >= 0:
+                arr[j+1] = arr[j]
+                j -= 1
+            arr[j+1] = key 
+    return arr
+
+print(insertionSort(arr))
+
